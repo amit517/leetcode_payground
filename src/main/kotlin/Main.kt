cb9cffd370ef9f2a21a81a25c793c298b8b41534
Amit Kundu <kundu.amit517@gmail.com>
2022-06-23 10:47:22 +0600

    191. Number of 1 Bits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,21 @@
 fun main() {
-    val arr: IntArray = intArrayOf(-1,1,0,-3,3)
     val solution = Solution()
-    println()
-
-    solution.productExceptSelf(arr).forEach {
-        println(it)
-    }
+    val numSt = Integer.parseUnsignedInt("11111111111111111111111111111101", 2)
+    println(numSt)
+    println(solution.hammingWeight(numSt))
 }
 
 class Solution {
-    fun productExceptSelf(nums: IntArray): IntArray {
-        val mutablelist = mutableListOf<Int>()
-        val multiplyValue = nums.reduce { acc, i ->
-            if (i != 0) {
-                acc * i
-            } else {
-                acc
+    fun hammingWeight(n: Int): Int {
+        val binary = Integer.toBinaryString(n)
+        var numberOfOne = 0
+        binary?.let {
+            for (element in it) {
+                if (element == '1') {
+                    numberOfOne += 1
+                }
             }
         }
-        for (i in nums.indices) {
-            if (nums[i] != 0) {
-                mutablelist.add(multiplyValue / nums[i])
-            } else {
-                mutablelist.add(multiplyValue)
-            }
-        }
-        return mutablelist.toIntArray()
+        return numberOfOne
     }
 }
