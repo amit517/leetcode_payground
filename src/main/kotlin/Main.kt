4cf0771e6c439f32aba7ec9b05b64203919794f1
Amit Kundu <kundu.amit517@gmail.com>
2022-08-30 14:38:51 +0600

    Max area
    Signed-off-by: Amit Kundu <kundu.amit517@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,53 @@
-import kotlin.math.pow
+import kotlin.math.max
 
 fun main() {
     val solution = Solution()
-    val numSt = Integer.parseUnsignedInt("11111111111111111111111111111101", 2)
-    println(solution.reverseBits(numSt))
+    val array = arrayOf(1,8,6,2,5,4,8,3,7)
+    println(solution.maxArea(array.toIntArray()))
 }
 
 class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
+
+    fun maxArea(height: IntArray): Int {
+        var maxArea = 0
+        var pointerA = 0
+        var pointerB = height.size - 1
+
+        while (pointerA < pointerB) {
+            if (height[pointerA] < height[pointerB]) {
+                maxArea = max(maxArea, height[pointerA] * (pointerB - pointerA))
+                pointerA+=1
+            } else {
+                maxArea = max(maxArea, height[pointerB] * (pointerB - pointerA))
+                pointerB-=1
+            }
+        }
+        return maxArea
+    }
+
+    fun subIntSummation(number: Int): Int {
+        var string = number.toString()
+
+        for (i in string.indices) {
+            for (j in (i + 1) until string.length) {
+                val sb = StringBuilder(string).also {
+                    val previousChar = string[i]
+                    println("previous char for index $i is $previousChar")
+
+                    it.setCharAt(i, string[j])
+                    it.setCharAt(j, previousChar)
+                }
+                val result = sb.toString()
+                println(result)
+            }
+        }
+
+
+        return 0
+    }
 
     fun reverseBits(n: Int): Int {
         val binary = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0')
