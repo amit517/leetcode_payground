54d89b076ef1a5c2f397a64d29045c4ca5f1776c
Amit Kundu <kundu.amit517@gmail.com>
2022-09-26 17:23:09 +0600

    Values in array in both way
    Signed-off-by: Amit Kundu <kundu.amit517@gmail.com>

--- src/main/kotlin/LinkeList.kt
+++ src/main/kotlin/LinkeList.kt
@@ -8,12 +8,39 @@ fun main() {
     b.next = c
     c.next = d
 
-    printLinkedListValue(a)
+//    printLinkedListValue(a)
+    println(getValueArrayFromLinkedlist(a))
 }
+
+fun getValueArrayFromLinkedlist(head: Node<Int>?): MutableList<Int>? {
+    // Non-recursive process
+
+    /*var mutableList = mutableListOf<Int>()
+
+    var currentNode: Node<Int>? = head
+
+    while (currentNode != null) {
+        mutableList.add(currentNode.value)
+        currentNode = currentNode.next
+    }*/
+
+    // Recursive Process
+    var mutableList = mutableListOf<Int>()
+    fillValues(head, mutableList)
+    return mutableList
+}
+
+fun fillValues(head: Node<Int>?, mutableList: MutableList<Int>): MutableList<Int>? {
+    if (head == null) return null
+    mutableList.add(head.value)
+    fillValues(head.next, mutableList)
+    return mutableList
+}
+
 
 fun printLinkedListValue(head: Node<Int>?) {
     // Non-recursive process
-    /*var currentNode: Node<Int>? = a
+    /*var currentNode: Node<Int>? = head
 
     while (currentNode != null) {
         println(currentNode.value)
