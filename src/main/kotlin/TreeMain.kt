e17caac9263a845faa8a2c4a39fa1b81429f10da
Amit Kundu <kundu.amit517@gmail.com>
2022-11-14 21:34:35 +0600

    Same Tree

--- src/main/kotlin/TreeMain.kt
+++ src/main/kotlin/TreeMain.kt
@@ -1,18 +1,29 @@
 import kotlin.math.max
 
 fun main() {
-    var a = TreeNode(3)
-    var b = TreeNode(9)
-    var c = TreeNode(20)
+    var a = TreeNode(1)
+    var b = TreeNode(1)
+    var c = TreeNode(2)
     var d = TreeNode(15)
     var e = TreeNode(7)
 
-    a.left = b
+    /*a.left = b
     a.right = c
     c.left = d
-    c.right = e
+    c.right = e*/
 
-    println(maxDepth(a))
+    b.left = null
+    b.right = c
+
+    println(isSameTree(a,b))
+}
+
+fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+    if (p == null && q == null) return true
+    if (q == null || p == null) return false
+    if (p.`val` != q.`val`) return false
+
+    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
 }
 
 fun maxDepth(root: TreeNode?): Int {
