d5cfd8dd652a855cd2f6d2fcd7f9bba74a3e9b88
Amit Kundu <amit.finland.01@gmail.com>
2024-03-26 06:48:02 +0200

    LC_347_topKFrequent solved

--- src/main/kotlin/TopicHashMap.kt
+++ src/main/kotlin/TopicHashMap.kt
@@ -16,4 +16,28 @@ class TopicHashMap {
         return intArrayOf()
     }
 
+    fun LC_347_topKFrequent(nums: IntArray, k: Int): IntArray {
+        var count = k
+
+        val hashMap = HashMap<Int, Int>()
+
+        nums.forEach {
+            hashMap[it] = hashMap.getOrDefault(it, 0) + 1
+        }
+        val mutableIntArray = mutableListOf<Int>()
+        var currentTop: Pair<Int, Int> = Pair(Int.MIN_VALUE, Int.MIN_VALUE)
+        while (count >= 1) {
+            for ((key, value) in hashMap) {
+                if (value > currentTop.first) {
+                    currentTop = Pair(value, key)
+                }
+            }
+
+            mutableIntArray.add(currentTop.second)
+            hashMap[currentTop.second] = Int.MIN_VALUE
+            currentTop = Pair(Int.MIN_VALUE, Int.MIN_VALUE)
+            count--
+        }
+        return mutableIntArray.toIntArray()
+    }
 }
