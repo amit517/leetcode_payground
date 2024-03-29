0c0689f9f726bad9b626a7322cbd2bee81a09c26
Amit Kundu <amit.finland.01@gmail.com>
2024-03-29 10:24:43 +0200

    LC_36_isValidSudoku solved

--- src/main/kotlin/TopicString.kt
+++ src/main/kotlin/TopicString.kt
@@ -1,11 +1,27 @@
 import java.util.*
+import kotlin.collections.HashSet
 import kotlin.collections.LinkedHashMap
 
 fun main() {
     val topicString = TopicString()
-    println(topicString.LC_387_firstUniqChar("leetcode"))
-    println(topicString.LC_387_firstUniqChar("loveleetcode"))
-    println(topicString.LC_387_firstUniqChar("aabb"))
+    val board = arrayOf(
+        charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+        charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+        charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+        charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+        charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+        charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+        charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+        charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+        charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+    )
+
+    val isValid = topicString.LC_36_isValidSudoku(board)
+    if (isValid) {
+        println("The Sudoku board is valid.")
+    } else {
+        println("The Sudoku board is invalid.")
+    }
 }
 
 class TopicString {
@@ -77,9 +93,9 @@ class TopicString {
     /**
      * Problem:
      * Having the function SearchingChallenge(str) take str parameter being passed and return 1 #ofBrackets if the brackets are correctly matched and each one is accounted for. Oterwise return 0. For example : if str is "(hello [world])(!)", then the output should be 1 3 because all the brackets are matched and there are 3 pairs of brackets, but if str is "((hello [world])" the the output should be 0 because the brackets do not correctly match up. Only "(", ")", "[", and "]" will be used as brackets. If str contains no brackets return 1.
-        val result1 = SearchingChallenge("(hello [world])(!)")  // Output: 1 3
-        val result2 = SearchingChallenge("((hello [world])")     // Output: 0
-        val result3 = SearchingChallenge("no brackets here")    // Output: 1 0
+    val result1 = SearchingChallenge("(hello [world])(!)")  // Output: 1 3
+    val result2 = SearchingChallenge("((hello [world])")     // Output: 0
+    val result3 = SearchingChallenge("no brackets here")    // Output: 1 0
      */
 
     fun SearchingChallenge(str: String): String {
@@ -108,6 +124,26 @@ class TopicString {
         }
 
         return if (stack.isEmpty()) "1 $numBrackets" else "0"
+    }
+
+    fun LC_36_isValidSudoku(board: Array<CharArray>): Boolean {
+
+        val seen = HashSet<String>()
+
+        for (i in board.indices) {
+            for (j in board.indices) {
+                val number = board[i][j]
+
+                if (number != '.') {
+                    if (!seen.add("$number in row $i") ||
+                        !seen.add("$number in column $j") ||
+                        !seen.add(number + " in block " + i/3 + "-" + j/3))
+                        return false
+                }
+            }
+        }
+
+        return true
     }
 
 }
