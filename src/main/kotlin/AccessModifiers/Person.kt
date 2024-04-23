bc467d52cc496cb35e07a86c4402cd2d5f2c032e
Amit Kundu <kundu.amit517@gmail.com>
2024-04-24 00:02:26 +0300

    OOP example updated

--- src/main/kotlin/AccessModifiers/Person.kt
+++ src/main/kotlin/AccessModifiers/Person.kt
@@ -5,12 +5,31 @@ open class Person {
     protected val protectedSalary = 500 // Can be accessible to it's child classes
     internal val officeEnvironment = "Only be accessible from with in the kotlin module"
     val workLocation = "Can be accessible from anywhere. By default kotlin fields are public and final"
+
+    protected fun printSalary(){
+        println(protectedSalary)
+    }
 }
 
 class Amit() : Person() {
-
+    init {
+        printSalary()
+        println(protectedSalary)
+    }
 }
 
+fun main(){
+
+    val amit = Amit()
+
+    println(amit.workLocation)
+    println(amit.officeEnvironment)
+
+    val person = Person()
+    
+    println(person.workLocation)
+
+}
 
 /* Protected can't be top level function. It can only be on the member function / methods */
 //protected class AmitInternal() : Person() {
